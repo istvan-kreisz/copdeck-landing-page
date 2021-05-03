611b684da651753391b62ad8db7157cb8533b717
kreiszistvan <kreiszdev@gmail.com>
2021-05-03 22:55:28 +0200

    v1 draft done

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -23,9 +23,13 @@ const Navbar = (props) => {
 							>
 								<div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
 									<div className="flex items-center justify-between w-full md:w-auto">
-										<a href="/">
+										<a className="flex items-center" href="/">
 											<span className="sr-only">Icon</span>
-											<img className="h-8 w-auto sm:h-10" src="sneaker.png" />
+											<img
+												className="h-14 sm:h-16 w-auto "
+												src="sneaker.png"
+											/>
+											<p className="ml-3 font-semibold text-lg">CopDeck</p>
 										</a>
 										<div className="-mr-2 flex items-center md:hidden">
 											<Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -67,7 +71,7 @@ const Navbar = (props) => {
 								<div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
 									<div className="px-5 pt-4 flex items-center justify-between">
 										<div>
-											<img className="h-8 w-auto" src="sneaker.png" alt="" />
+											<img className="h-14 w-auto" src="sneaker.png" alt="" />
 										</div>
 										<div className="-mr-2">
 											<Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
