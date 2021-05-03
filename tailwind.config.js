523c60f52379a60809caaa626663b300cf7f3da3
kreiszistvan <kreiszdev@gmail.com>
2021-05-03 10:40:13 +0200

    fonts & setup work

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				body: ['LibreFranklin-VariableFont', ...defaultTheme.fontFamily.sans],
+				sans: ['LibreFranklin-VariableFont', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/forms')],
+}
